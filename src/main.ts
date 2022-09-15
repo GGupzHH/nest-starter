9436b5dda8f5caf009fe640ba01a2f9a14f0d0ce
赵雅国 <zhao.yaguo@shinewingdigital.com>
2022-09-15 17:40:07 +0800

    feat: modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/Bank/app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { gerLocalhost } from './utils';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const { PORT } = process.env;
+
+  // http-exception-filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(PORT);
+
+  const localhost = gerLocalhost();
+
+  console.log('\n', '🚀   Nest Starter Server\n');
+  console.log(`> Local: http://localhost:${PORT}`);
+  console.log(`> Network: http://${localhost}:${PORT}\n`);
 }
+
 bootstrap();
--- src/modules/Bank/app.controller.ts
+++ src/modules/Bank/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/hoem')
+  getHello1(): string {
+    return this.appService.getHello();
+  }
 }
